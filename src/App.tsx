26f0102478c839c510dc6b577bc9e40ed27a486c
EveFon Wu <evefonwu@gmail.com>
2021-02-03 16:34:56 -0500

    Move BrowserRouter inside App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Snacks from './Snacks'
 
 function App() {
   return (
-    <div>    
-      <Switch>
-        <Route exact path="/" component={Snacks} />
-      </Switch>            
+    <div>   
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Snacks} />
+        </Switch>      
+      </BrowserRouter>       
     </div>
   );
 }
